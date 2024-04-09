fc3bb12b9fd8666d938846c35460d59b8f83e254
jordan <jgruel1@madisoncollege.edu>
2024-04-08 20:36:46 -0500

    removing test responses

--- src/main/java/edu/matc/rest/ResponseHolidayAll.java
+++ src/main/java/edu/matc/rest/ResponseHolidayAll.java
@@ -36,44 +36,4 @@ public class ResponseHolidayAll {
         return Response.status(200).entity(json).build();
     }
 
-    @Path("/plainText")
-    @GET
-    @Produces("text/plain")
-    public Response getPlainMessage(@Context HttpHeaders requestHeaders) {
-        /**
-        GenericDao<Holiday> holidayDao = new GenericDao<>(Holiday.class);
-        List<Holiday> holidays = holidayDao.getAll();
-        //Generate strings of holidays
-        StringBuilder builder = new StringBuilder();
-        for (Holiday holiday : holidays) {
-            builder.append(holiday.toString());
-            builder.append("\n");
-        }
-        String output = builder.toString();*/
-
-        String output = requestHeaders.getRequestHeader("Accept").get(0);
-        return Response.status(200).entity(output).build();}
-
-    @Path("/plainText")
-    @GET
-    @Produces("text/html")
-    public Response getHtmlMessage(@Context HttpHeaders requestHeaders) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("<html><title>Titled</title><body>");
-
-        builder.append(requestHeaders.getRequestHeader("Accept").get(0));
-        builder.append("</body></html>");
-        String output = builder.toString();
-        return Response.status(200).entity(output).build();}
-
-    @GET
-    @Path("/test1")
-    public String allHeaders(@Context  HttpHeaders headers) {
-        MultivaluedMap<String, String> rh = headers.getRequestHeaders();
-        String str = rh.entrySet()
-                .stream()
-                .map(e -> e.getKey() + " = " + e.getValue())
-                .collect(Collectors.joining("<br/>"));
-        return str;
-    }
 }

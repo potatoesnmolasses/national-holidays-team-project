66d5949092bcd6bb4f955ca454a71cace6ef2db5
jordan <jgruel1@madisoncollege.edu>
2024-04-09 22:54:55 -0500

    merging changes jg

--- src/main/java/edu/matc/rest/RequestHoliday.java
+++ src/main/java/edu/matc/rest/RequestHoliday.java
@@ -1,9 +1,7 @@
 package edu.matc.rest;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Application;
-import javax.ws.rs.ext.Provider;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,7 +13,6 @@ public class RequestHoliday extends Application {
     //The method returns a non-empty collection with classes, that must be included in the published JAX-RS application
     @Override
     public Set<Class<?>> getClasses() {
-
         HashSet h = new HashSet<Class<?>>();
         h.add(ResponseHolidayAll.class);
         h.add(ResponseHolidayById.class);
